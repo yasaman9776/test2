44f754927f52ee8e0cb8c8b6722f72162a78fd6b
yasaman <yasii30.yts@gmail.com>
2024-04-22 15:43:29 +0330

    2

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,41 +1,61 @@
 import styled from 'styled-components'
-import { CokatexColors } from '../helper/colors';
+import {CokatexColors} from '../helper/colors';
 const handleColorType = color => {
-     switch (color) {
-      case "primary":
-        return " background: #ffb921;";
-       case "danger":
-         return " background: #f56342;";
-     default:
-        return {backgroundColor: `${CokatexColors.navyBlue}`}
-       
-     }
-   
-  };
-  const handleHoverColor = color =>{
     switch (color) {
-      case "primary":
-        return " background: #ffb921;";
-       case "danger":
-         return " background: #f56342;";
-     default:
-        return {backgroundColor:`${CokatexColors.lightBlue}`}
-       
-     }
-  }
-    export const DefaultButton = styled.button`
-    ${({ color }) => handleColorType(color)};
-    border: none;
-    padding: 10px 40px;
-    color:#fff;
-    width:120px;
-    border-radius:10px;
-    font-family:"IRANYekanBold";
-    &:hover{
-      ${({ color }) => handleHoverColor(color)};
+        case "primary":
+            return " background: #ffb921;";
+        case "danger":
+            return " background: #f56342;";
+        default:
+            return {backgroundColor: `${CokatexColors.navyBlue}`}
+
     }
-    `
+
+};
+const handleHoverColor = color => {
+    switch (color) {
+        case "primary":
+            return " background: #ffb921;";
+        case "danger":
+            return " background: #f56342;";
+        default:
+            return {backgroundColor: `${CokatexColors.lightBlue}`}
+
+    }
+}
+export const DefaultButton = styled.button `
+    ${ ({
+    color}) => handleColorType(color)};
+    border: none;
+    
+    color:#fff;
+    cursor:pointer;
+    border-radius:5px;
+    font-family:"IRANYekanBold";
+    box-shadow:0px 8px 16px 0px rgba(12, 13, 66, 0.34);
+    &:hover{
+      ${ ({
+        color}) => handleHoverColor(color)};
+    };
+    i {
+      margin-right: 5px;
+    }
+    ` 
     export const ExtendedButton = styled(DefaultButton)`
     display: block;
     width: 100vw;
+    ` 
+    export const LargeButton = styled(DefaultButton)`
+    padding: 10px 40px;
+    ` 
+    export const MediumButton = styled(DefaultButton)`
+    padding: 8px 28px;
+    ` 
+    export const SmallButton = styled(DefaultButton)`
+    padding: 4px 12px;
+    ` 
+    export const IconButton = styled(DefaultButton)`
+    display:flex;
+    align-items:center;
+    padding: 10px 20px;
     `
--- src/pages/AllButtons.jsx
+++ src/pages/AllButtons.jsx
@@ -1,18 +1,149 @@
-import { DefaultButton,ExtendedButton } from "../components/Button";
+import {
+    DefaultButton,
+    ExtendedButton,
+    LargeButton,
+    MediumButton,
+    SmallButton,
+    IconButton
+} from "../components/Button";
 
+import SaveIcon from "../assets/icon/SaveIcon";
+const AllButtons = ({props}) => {
 
-const AllButtons = () =>{
+    return ( 
+    <> <div style={{
+        padding: 50
+    }}>
 
-    return(
-        <>
-        <div style={{padding:50}}>
-            
-            <DefaultButton>سلام</DefaultButton>
-
+        <div
+            style={{
+            display: 'flex',
+            marginBottom: 50
+        }}>
+            <div
+                style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: "10px 50px"
+            }}>
+                <span
+                    style={{
+                    fontFamily: 'IRANYekanBold',
+                    color: "#000",
+                    marginBottom: 10
+                }}>
+                    Large Button
+                </span>
+                <LargeButton >
+                    کلیک کنید !
+                </LargeButton>
+            </div>
+            <div
+                style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: "10px 50px"
+            }}>
+                <span
+                    style={{
+                    fontFamily: 'IRANYekanBold',
+                    color: "#000",
+                    marginBottom: 10
+                }}>
+                    Medium Button
+                </span>
+                <MediumButton>
+                    کلیک کنید !
+                </MediumButton>
+            </div>
+            <div
+                style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: "10px 50px"
+            }}>
+                <span
+                    style={{
+                    fontFamily: 'IRANYekanBold',
+                    color: "#000",
+                    marginBottom: 10
+                }}>
+                    Small Button
+                </span>
+                <SmallButton>
+                    کلیک کنید !
+                </SmallButton>
+            </div>
         </div>
-        
-        
-        </>
+        <div
+            style={{
+            display: 'flex',
+            marginBottom: 50
+        }}>
+            <div
+                style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: "10px 50px"
+            }}>
+                <span
+                    style={{
+                    fontFamily: 'IRANYekanBold',
+                    color: "#000",
+                    marginBottom: 10
+                }}>
+                    Large Button
+                </span>
+                <IconButton>
+                    کلیک کنید !
+                    <SaveIcon
+                        height={15}
+                        width={15}
+                        color={'#fff'}
+                        style={{
+                        marginLeft: 10
+                    }}/>
+
+                </IconButton>
+            </div>
+            <div
+                style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: "10px 50px"
+            }}>
+                <span
+                    style={{
+                    fontFamily: 'IRANYekanBold',
+                    color: "#000",
+                    marginBottom: 10
+                }}>
+                    Medium Button
+                </span>
+                <MediumButton>
+                    کلیک کنید !
+                </MediumButton>
+            </div>
+            <div
+                style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: "10px 50px"
+            }}>
+                <span
+                    style={{
+                    fontFamily: 'IRANYekanBold',
+                    color: "#000",
+                    marginBottom: 10
+                }}>
+                    Small Button
+                </span>
+                <SmallButton>
+                    کلیک کنید !
+                </SmallButton>
+            </div>
+        </div>
+    </div> </>
     )
 }
 
